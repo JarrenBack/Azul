08e18734f290625cd8c06cca18d47f415d42f3bf
JarrenBack <jarren.back@gmail.com>
2018-10-07 12:27:59 -0500

    Attempting to drag and drop

---
+++ AzulUnity/Assets/DragScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DragScript : MonoBehaviour
+{
+
+    private Vector3 dragPosition;
+
+    void OnMouseDrag()
+    {
+        dragPosition = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.z);
+        dragPosition = Camera.main.ScreenToWorldPoint(dragPosition);
+        dragPosition.y = 0.0f; // assuming 2D game
+    }
+
+    void Update()
+    {
+        transform.position = dragPosition;
+    }
+}
---
+++ AzulUnity/Assets/DragTransform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+class DragTransform : MonoBehaviour
+{
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
