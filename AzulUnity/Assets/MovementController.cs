f7deac1054de1646604348bc9babb405a18cab27
JarrenBack <jarren.back@gmail.com>
2018-10-07 14:24:51 -0500

    Added canvas and drag and drop

---
+++ AzulUnity/Assets/MovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class MovementController : MonoBehaviour
+{
+    Vector3 startPos;
+    Vector3 startPosOfCube;
+    Vector3 dist;
+
+    void OnMouseDown()
+    {
+        startPosOfCube = transform.position;
+        startPos = Camera.main.WorldToScreenPoint(transform.position);
+        dist = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, startPosOfCube.y, startPos.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 lastPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPos.z);
+        transform.position = Camera.main.ScreenToWorldPoint(lastPos) + dist;
+    }
+}
---
+++ AzulUnity/Assets/ShowHalo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHalo : MonoBehaviour {
+
+    public Behaviour halos;
+
+    private void Start()
+    {
+        Debug.Log("Start triggered from ShowHalo");
+        halos = (Behaviour)gameObject.GetComponent("Halo");
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("OnMouseEnter triggered");
+        halos.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("OnMouseExit triggered");
+        halos.enabled = false;
+    }
+}
